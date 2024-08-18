031cdade1336d68ebd0d949eaae533930825f191
miltwiri <miltonelim64@gmail.com>
2024-08-18 15:21:40 +0300

    sec

---
+++ bankapp/src/main/java/com/example/bankapp/code/config/SecurityConstants.java
@@ -0,0 +1,6 @@
+package com.example.bankapp.code.config;
+
+public interface SecurityConstants {
+    // public static final String JWT_KEY ="secretshimbhu$77$kumawat~skking99~";
+	// public static final String JWT_HEADER ="Authorization";
+}
--- bankapp/src/main/java/com/example/bankapp/code/config/WebSecurityConfig.java
+++ bankapp/src/main/java/com/example/bankapp/code/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.bankapp.code.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
